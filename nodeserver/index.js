517f04df59ba09923fee94d803407a9d359126cf
vinay kumar kashyap <120709710+Vinaykashyap1@users.noreply.github.com>
2024-04-30 12:59:45 +0530

    Add files via upload

---
+++ nodeserver/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+const PORT = process.env.PORT || 8000;
+
+const users = {};
+
+io.on('connection', socket => {
+  socket.on('new-user-joined', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  });
+
+  socket.on('send', message => {
+    socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('left', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
